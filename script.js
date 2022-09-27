7e920fe28ad32eb55e1df277a9f2cef414a49c6c
Joe Massey <joem29@hotmail.co.uk>
2022-09-27 17:24:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,542 @@
+var MaleIBMIDict = {
+  0: 13.281,
+  1: 17.644,
+  2: 16.655,
+  3: 16.133,
+  4: 15.752,
+  5: 15.547,
+  6: 15.498,
+  7: 15.564,
+  8: 15.748,
+  9: 16.037,
+  10: 16.423,
+  11: 16.892,
+  12: 17.433,
+  13: 18.037,
+  14: 18.675,
+  15: 19.317,
+  16: 19.938,
+  17: 20.519,
+  18: 21.052,
+  19: 21.54,
+  20: 21.986,
+  21: 22.4,
+  22: 22.788,
+  23: 23.154
+};
+
+var FemaleIBMIDict = {
+  0: 13.028,
+  1: 17.245,
+  2: 16.336,
+  3: 15.881,
+  4: 15.656,
+  5: 15.483,
+  6: 15.485,
+  7: 15.677,
+  8: 15.993,
+  9: 16.399,
+  10: 16.898,
+  11: 17.478,
+  12: 18.117,
+  13: 18.772,
+  14: 19.395,
+  15: 19.955,
+  16: 20.438,
+  17: 20.847,
+  18: 21.19,
+  19: 21.482,
+  20: 21.735,
+  21: 21.961,
+  22: 22.17,
+  23: 22.364
+};
+
+var Mintercept = {
+  4: -10.2567,
+  4.5: -10.719,
+  5: -11.0213,
+  5.5: -11.1556,
+  6: -11.1138,
+  6.5: -11.0221,
+  7: -10.9984,
+  7.5: -11.0214,
+  8: -11.0696,
+  8.5: -11.122,
+  9: -11.1571,
+  9.5: -11.1405,
+  10: -11.038,
+  10.5: -10.8286,
+  11: -10.4917,
+  11.5: -10.0065,
+  12: -9.3522,
+  12.5: -8.6055,
+  13: -7.8632,
+  13.5: -7.1348,
+  14: -6.4299,
+  14.5: -5.7578,
+  15: -5.1282,
+  15.5: -4.5092,
+  16: -3.9292,
+  16.5: -3.4873,
+  17: -3.283,
+  17.5: -3.4156
+};
+
+var MStature = {
+  4: 1.23812,
+  4.5: 1.15964,
+  5: 1.10674,
+  5.5: 1.0748,
+  6: 1.05923,
+  6.5: 1.05542,
+  7: 1.05877,
+  7.5: 1.06467,
+  8: 1.06853,
+  8.5: 1.06572,
+  9: 1.05166,
+  9.5: 1.02174,
+  10: 0.97135,
+  10.5: 0.89589,
+  11: 0.81239,
+  11.5: 0.74134,
+  12: 0.68325,
+  12.5: 0.63869,
+  13: 0.60818,
+  13.5: 0.59228,
+  14: 0.59151,
+  14.5: 0.60643,
+  15: 0.63757,
+  15.5: 0.68548,
+  16: 0.75069,
+  16.5: 0.83375,
+  17: 0.9352,
+  17.5: 1.05558
+};
+
+var MWeightLB = {
+  4: -0.0087235,
+  4.5: -0.0074454,
+  5: 0.0064778,
+  5.5: -0.005776,
+  6: -0.0052947,
+  6.5: -0.0049892,
+  7: -0.0048144,
+  7.5: -0.047256,
+  8: -0.0046778,
+  8.5: -0.0046261,
+  9: -0.045254,
+  9.5: -0.0043311,
+  10: -0.0039981,
+  10.5: -0.0034814,
+  11: -0.002905,
+  11.5: -0.0024167,
+  12: -0.0020076,
+  12.5: -0.0016681,
+  13: -0.0013895,
+  13.5: -0.0011624,
+  14: -0.0009776,
+  14.5: -0.0008261,
+  15: -0.0006988,
+  15.5: -0.0005863,
+  16: -0.0004795,
+  16.5: -0.0003695,
+  17: -0.000247,
+  17.5: -0.0001027
+};
+
+var MMidParentStature = {
+  4: 0.50286,
+  4.5: 0.52887,
+  5: 0.53919,
+  5.5: 0.53691,
+  6: 0.52513,
+  6.5: 0.50692,
+  7: 0.48538,
+  7.5: 0.46361,
+  8: 0.44469,
+  8.5: 0.43171,
+  9: 0.42776,
+  9.5: 0.43593,
+  10: 0.45932,
+  10.5: 0.50101,
+  11: 0.54781,
+  11.5: 0.58409,
+  12: 0.60927,
+  12.5: 0.62279,
+  13: 0.62407,
+  13.5: 0.61253,
+  14: 0.58762,
+  14.5: 0.54875,
+  15: 0.49536,
+  15.5: 0.42687,
+  16: 0.34271,
+  16.5: 0.24231,
+  17: 0.1251,
+  17.5: -0.0095
+};
+
+var Fintercept = {
+  4: -8.1325,
+  4.5: -6.47656,
+  5: -5.13582,
+  5.5: -4.13791,
+  6: -3.51039,
+  6.5: -3.14322,
+  7: -2.87645,
+  7.5: -2.66291,
+  8: -2.45559,
+  8.5: -2.20728,
+  9: -1.87098,
+  9.5: -1.0633,
+  10: 0.33468,
+  10.5: 1.97366,
+  11: 3.50436,
+  11.5: 4.57747,
+  12: 4.84365,
+  12.5: 4.27869,
+  13: 3.21417,
+  13.5: 1.83456,
+  14: 0.32425,
+  14.5: -1.13224,
+  15: -2.35055,
+  15.5: -3.10326,
+  16: -3.17885,
+  16.5: -2.41657,
+  17: -0.65579,
+  17.5: 2.26429
+};
+
+var FStature = {
+  4: 1.24768,
+  4.5: 1.22177,
+  5: 1.19932,
+  5.5: 1.1788,
+  6: 1.15866,
+  6.5: 1.13737,
+  7: 1.11342,
+  7.5: 1.08525,
+  8: 1.05135,
+  8.5: 1.01018,
+  9: 0.9602,
+  9.5: 0.89989,
+  10: 0.82771,
+  10.5: 0.74213,
+  11: 0.67173,
+  11.5: 0.6415,
+  12: 0.64452,
+  12.5: 0.67386,
+  13: 0.7226,
+  13.5: 0.78383,
+  14: 0.85062,
+  14.5: 0.91605,
+  15: 0.97319,
+  15.5: 1.01514,
+  16: 1.03496,
+  16.5: 1.02573,
+  17: 0.98054,
+  17.5: 0.89246
+};
+
+var FWeightLB = {
+  4: -0.019435,
+  4.5: -0.018519,
+  5: -0.01753,
+  5.5: -0.016484,
+  6: -0.0154,
+  6.5: -0.014294,
+  7: -0.013184,
+  7.5: -0.012086,
+  8: -0.011019,
+  8.5: -0.009999,
+  9: -0.009044,
+  9.5: -0.008171,
+  10: -0.007397,
+  10.5: -0.006739,
+  11: -0.006136,
+  11.5: -0.005518,
+  12: -0.004894,
+  12.5: -0.004272,
+  13: -0.003661,
+  13.5: -0.003067,
+  14: -0.0025,
+  14.5: -0.001967,
+  15: -0.001477,
+  15.5: -0.001037,
+  16: -0.000655,
+  16.5: -0.00034,
+  17: -0.0001,
+  17.5: 0.000057
+};
+
+var FMidParentStature = {
+  4: 0.44774,
+  4.5: 0.41381,
+  5: 0.38467,
+  5.5: 0.36039,
+  6: 0.34105,
+  6.5: 0.32672,
+  7: 0.31748,
+  7.5: 0.3134,
+  8: 0.31457,
+  8.5: 0.32105,
+  9: 0.33291,
+  9.5: 0.35025,
+  10: 0.37312,
+  10.5: 0.40161,
+  11: 0.42042,
+  11.5: 0.41686,
+  12: 0.3949,
+  12.5: 0.3585,
+  13: 0.31163,
+  13.5: 0.25826,
+  14: 0.20235,
+  14.5: 0.14787,
+  15: 0.0988,
+  15.5: 0.05909,
+  16: 0.03272,
+  16.5: 0.02364,
+  17: 0.03584,
+  17.5: 0.07327
+};
+
+function inputLineTemplate(lineCount){
+  var iStr =  `
+    <th> ` + lineCount +` </th>
+    <th><select class='Gender' value=''>
+        <option>Male</option>
+        <option>Female</option>
+      </select>
+    </th>
+    <th><input type='number' min="0" step="1" oninput="validity.valid||(value=parseInt(value));" class='Age' value=''></th>
+    <th><input type='number' class='Height' value=''></th>
+    <th><input type='number' class='Weight' value=''></th>
+    <th><input type='number' class='MidParent' value=''></th>
+  `;
+  return iStr;
+}
+
+function outputLineTemplate(lineCount){
+  var oStr =  ` 
+    <th> ` + lineCount + `</th>
+    <th class='BMIOUT'></th>
+    <th class='IBMIOUT'></th>
+    <th class='IBWOUT'></th>
+    <th class='PerWFHOUT'></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th class='PreHeightOUT'></th>
+    <th class='PerPreHeightOUT'></th>
+    <th class='PubStageOUT'></th>
+  `;
+  return oStr;
+}
+
+function BMI(W, H) {
+  return W / (H * H);
+}
+
+function IBW(IBMI, H) {
+  return IBMI * (H * H);
+}
+
+function PredictedHeight(Gender, Age, Height, Weight, MidParent) {
+  if (Gender == "Male") {
+    A = Mintercept[Age];
+    B = MStature[Age];
+    C = MWeightLB[Age];
+    D = MMidParentStature[Age];
+  } else if (Gender == "Female") {
+    A = Fintercept[Age];
+    B = FStature[Age];
+    C = FWeightLB[Age];
+    D = FMidParentStature[Age];
+  }
+
+  return (
+    (A +
+      (B * Height * 100) / 2.54 +
+      C * Weight * 2.20462 +
+      (D * MidParent * 100) / 2.54) /
+    39.37
+  );
+}
+
+function HeightPreToString(H) {
+  if (H > 0 && H < 0.85) {
+    return "Pre-Pubertal";
+  } else if (H >= 0.85 && H < 0.9) {
+    return "Early Pubertal";
+  } else if (H >= 0.9 && H < 0.95) {
+    return "Mid-Pubertal";
+  } else if (H > 0.95) {
+    return "Late-Pubertal";
+  }
+
+  return "Error";
+}
+
+function UpdateForm(QIForm, QOForm) {
+  var CSVRunner = "";
+  var BMIOUT = QOForm.querySelector(".BMIOUT");
+  var IBMIOUT = QOForm.querySelector(".IBMIOUT");
+  var IBWOUT = QOForm.querySelector(".IBWOUT");
+  var PerWFHOUT = QOForm.querySelector(".PerWFHOUT");
+  var PreHeightOUT = QOForm.querySelector(".PreHeightOUT");
+  var PerPreHeightOUT = QOForm.querySelector(".PerPreHeightOUT");
+  var PubStageOUT = QOForm.querySelector(".PubStageOUT");
+
+  var AgeIn = QIForm.querySelector(".Age");
+  var HeightIn = QIForm.querySelector(".Height");
+  var WeightIN = QIForm.querySelector(".Weight");
+  var MidParentIN = QIForm.querySelector(".MidParent");
+  var GenderIN = QIForm.querySelector(".Gender");
+  var Gender = GenderIN.options[GenderIN.selectedIndex].text;
+
+  var LBMI = BMI(WeightIN.value, HeightIn.value)
+  BMIOUT.innerHTML = LBMI.toFixed(2);
+  CSVRunner = CSVRunner + LBMI + ' , ';
+
+  if (Gender == "Male") {
+    var LIBMI = MaleIBMIDict[AgeIn.value];
+  } else if (Gender == "Female") {
+    var LIBMI = FemaleIBMIDict[AgeIn.value];
+  }
+  IBMIOUT.innerHTML = LIBMI;
+  CSVRunner = CSVRunner + LIBMI + ' , ';
+  
+  var LIBW = IBW(MaleIBMIDict[AgeIn.value], HeightIn.value);
+  IBWOUT.innerHTML = LIBW.toFixed(2);
+  CSVRunner = CSVRunner + LIBW + ' , ';
+  
+  var LPerWFH = (
+      (WeightIN.value / IBW(MaleIBMIDict[AgeIn.value], HeightIn.value)) *
+      100
+    );
+  PerWFHOUT.innerHTML =
+   LPerWFH.toFixed(2) + "%";
+  CSVRunner = CSVRunner + LPerWFH + ' , ';
+  
+  CSVRunner = CSVRunner + ' , ';
+  CSVRunner = CSVRunner  + ' , ';
+  CSVRunner = CSVRunner  + ' , ';
+
+  var LPreHeight = PredictedHeight(
+    Gender,
+    AgeIn.value,
+    HeightIn.value,
+    WeightIN.value,
+    MidParentIN.value
+  );
+  PreHeightOUT.innerHTML = LPreHeight.toFixed(3);
+  CSVRunner = CSVRunner + LPreHeight + ' , ';
+  
+
+  var PerPreH =
+    HeightIn.value /
+    LPreHeight;
+
+  PerPreHeightOUT.innerHTML = PerPreH.toFixed(2);
+  CSVRunner = CSVRunner + PerPreH + ' , ';
+
+  
+  PubStageOUT.innerHTML = HeightPreToString(PerPreH);
+  CSVRunner = CSVRunner + HeightPreToString(PerPreH);
+  
+  return CSVRunner;
+}
+
+var I_LineZone = document.getElementById("InputTable");
+var O_LineZone = document.getElementById("OutputTable");
+
+var lineCount = 1;
+
+function UpdateForms(lineCount) {
+  var CSV = "";
+  var n = 1;
+  while(n <= lineCount){
+    var QIForm = document.getElementById("I_" + n);
+    var QOForm = document.getElementById("O_" + n);
+    CSV = CSV + UpdateForm(QIForm, QOForm) + '<br>';
+    n++;
+  }
+  document.getElementById("CSV").innerHTML = 'BMI,	Ideal BMI,	Ideal Body Weight	,% WFH,	Percentile BMI,	Percentile Weight,	Percentile Height,	Predicted Height,	% Predicted Height,	Pubertal Stage <br>' + CSV;
+}
+
+function AddLine(lineCount, I_LineZone, O_LineZone){
+  var toAdd = document.getElementById("LineAmount").value;
+  while(toAdd > 0){
+    lineCount ++; 
+
+      var newILine = document.createElement('tr');
+      newILine.setAttribute('id', 'I_'+lineCount);
+      newILine.innerHTML = inputLineTemplate(lineCount);
+      I_LineZone.appendChild(newILine);
+
+      var newOLine = document.createElement('tr');
+      newOLine.setAttribute('id', 'O_'+lineCount);
+      newOLine.innerHTML = outputLineTemplate(lineCount);
+      O_LineZone.appendChild(newOLine);
+
+    //I_LineZone.innerHTML =  I_LineZone.innerHTML + inputLineTemplate(lineCount);
+    //O_LineZone.innerHTML = O_LineZone.innerHTML + outputLineTemplate(lineCount);
+    UpdateForms(lineCount);
+    toAdd --;
+  }
+  return lineCount;
+}
+
+I_LineZone.addEventListener("change", (event) => {
+  UpdateForms(lineCount);
+});
+UpdateForms(lineCount);
+
+
+
+
+
+
+
+function CSVInput(lineCount, I_LineZone, O_LineZone){
+  var toImport = document.getElementById("CSVInput").value;
+  var lines = toImport.split('\n');
+  
+  for (const line of lines) {
+    lineCount ++ 
+    
+    var parts = line.split(',');
+    
+    var newILine = document.createElement('tr');
+      newILine.setAttribute('id', 'I_'+lineCount);
+    
+    var iStr =  `
+      <th> ` + lineCount +` </th>
+      <th><select class='Gender' value='`+ parts[0].trim() +`'>
+          <option>Male</option>
+          <option>Female</option>
+        </select>
+      </th>
+      <th><input type='number' min="0" step="1" oninput="validity.valid||(value=parseInt(value));" class='Age' value='`+ parts[1].trim()  +`'></th>
+      <th><input type='number' class='Height' value='`+ parts[2].trim()  +`'></th>
+      <th><input type='number' class='Weight' value='`+ parts[3].trim()  +`'></th>
+      <th><input type='number' class='MidParent' value='`+ parts[4].trim()  +`'></th>
+    `;
+    
+    
+    
+    
+      newILine.innerHTML = iStr;
+      I_LineZone.appendChild(newILine);
+    
+    
+    
+    var newOLine = document.createElement('tr');
+      newOLine.setAttribute('id', 'O_'+lineCount);
+      newOLine.innerHTML = outputLineTemplate(lineCount);
+      O_LineZone.appendChild(newOLine);
+    
+  }
+  UpdateForms(lineCount);
+  return lineCount;
+}
